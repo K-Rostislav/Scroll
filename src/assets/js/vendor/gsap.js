80212c0804fd55678b174596df979a8bfa2813c8
K-Rostislav <moregan123456789@gmail.com>
2023-03-21 21:23:31 +0300

    fix

--- src/assets/js/vendor/gsap.js
+++ src/assets/js/vendor/gsap.js
@@ -1,30 +1,10 @@
 import {gsap, ScrollTrigger} from "gsap/all.js"
-import LocomotiveScroll from "locomotive-scroll"
+import Lenis from "@studio-freight/lenis"
+import rallax from "rallax.js"
 
 gsap.registerPlugin(ScrollTrigger)
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    lerp: 0.05,
-    scrollbarClass: '.main',
-})
-
-ScrollTrigger.scrollerProxy('.main', {
-    scrollTop(value) {
-        return arguments.length ? scroll.scrollTo(value, {duration: 0, disableLerp: true}) : scroll.scroll.instance.scroll.y
-    },
-    getBoundingClientRect() {
-      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight}
-    },
-    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
-scroll.on("scroll", ScrollTrigger.update)
-ScrollTrigger.defaults({ scroller: ".main" })
-ScrollTrigger.addEventListener("refresh", () => scroll.update())
-
 const itemsL = document.querySelectorAll('.portfolio__left-block .portdolio__item')
-
 for (let item of itemsL) {
     gsap.fromTo(item, {x: -120, opacity: 0}, {
         x: 0,
@@ -35,9 +15,7 @@ for (let item of itemsL) {
         }
     })
 }
-
 const itemsR = document.querySelectorAll('.portfolio__right-block .portdolio__item')
-
 for (let item of itemsR) {
     gsap.fromTo(item, {x: 120, opacity: 0}, {
         x: 0,
@@ -48,3 +26,17 @@ for (let item of itemsR) {
         }
     })
 }
+
+const lenis = new Lenis({
+    lerp: 0.05,
+})
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+const img = document.querySelector('.intro__img')
+rallax(img, {
+    speed: 0.2,
+})
